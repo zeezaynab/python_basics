a92575e7955c0c205e41ef86086a69f44c264788
zeezaynab <151203984+zeezaynab@users.noreply.github.com>
2024-02-20 12:40:30 +0500

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,29 @@
+#project no.1
+#MADLIB
+#ways to do string concatenation 
+#print("subscribe to"+youtuber)
+#print(f"subscribe to {youtuber}")
+#print("subscribe to {}".format(youtuber))
+#here the 3rd method is called an f string method just prepend an F before the command prompt.
+#youtuber=input("youtuber")
+#print("subscribe to "+youtuber)
+
+#f-string:
+name=input("name")
+age=input("age")
+favourite_color=input("favourite_colour")
+
+madlib1=f"Hi! my name is {name}.I am {age} years old and i love {favourite_color} the best."
+print(madlib1)
+
+#string concatenation:
+name2=input("name2")
+fav_flower=input("fav_flower")
+studying=input("studying")
+
+madlib2="hi my name is "+name2+" i love "+fav_flower+" i am currently a 1st year student at COMSATS university studying "+studying
+print(madlib2)
+
+#facing an issue with name2 input.
+#nvm issue resolved.
+
