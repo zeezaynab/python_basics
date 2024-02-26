6c745d7dfd8c6364be15133f4d568d83ff0979de
zeezaynab <151203984+zeezaynab@users.noreply.github.com>
2024-02-26 15:45:05 +0500

    Add files via upload

---
+++ digits summation.py
@@ -0,0 +1,21 @@
+#i=str(input("Enter a number between 0 and 1000: "))
+#if 0<=int(i)<=9:
+ #a = int(i[0])
+ #print(a)
+#elif 10<=int(i)<=99:
+ #a=int(i[0])
+ #b=int(i[1])
+ #print(a+b)
+#elif 100<=int(i)<=999:
+ #a=int(i[0])
+ #b=int(i[1])
+ #c=int(i[2])
+ #print(a+b+c)
+#elif int(i)==1000:
+ #a=int(i[0])
+ #print(a)
+#else:
+ #print("invalid entry.")
+
+
+
